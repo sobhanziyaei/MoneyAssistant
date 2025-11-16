c767f6b000384daaa21ee100ade851aa9c8283de
Sobhan <ziaey@gmail.com>
2025-11-16 15:55:49 +0330

    chore: add Home and SideMenu components with initial templates and tests

---
+++ src/app/core/components/side-menu/side-menu.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-side-menu',
+  imports: [],
+  templateUrl: './side-menu.component.html',
+  styleUrl: './side-menu.component.scss',
+})
+export class SideMenuComponent {
+
+}
