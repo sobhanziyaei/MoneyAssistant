ddb6bb9444ee645e2312236a14a213956f06bfed
Sobhan <ziaey@gmail.com>
2025-11-16 17:01:29 +0330

    chore: update routing configuration to load HomeComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () => import('./core/components/home/home.component').then(m => m.HomeComponent),
+        children: [
+            
+        ]
+    },
+
+];
